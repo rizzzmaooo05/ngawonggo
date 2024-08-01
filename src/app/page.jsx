a683d9d1e8287d756a439ae61d7dd853a51f822c
Rizzz Maooo <rizqonmaulana05@gmail.com>
2024-08-01 10:57:18 +0700

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,7 +9,7 @@ export default function Home() {
     <>
       {/* <Navbar /> */}
       <Main />
-      {/* <Articles /> */}
+      <Articles />
       {/* <AboutUs /> */}
       {/* <Contacts/> */}
     </>
