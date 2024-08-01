94f1a46ac941417fc31ac4ffaf69c5eda9844020
Rizzz Maooo <rizqonmaulana05@gmail.com>
2024-08-01 10:42:19 +0700

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className={dosis.className}>{children}</body>
+      <body className={`overflow-x-hidden ${dosis.className}`}>{children}</body>
     </html>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,25 +6,25 @@ const poppins = Poppins({subsets: ['latin'], weight: "900"})
 
 export default function Navbar() {
   return (
-    <nav className="flex flex-col bg-[#222937] text-white px-10 py-4 justify-between items-center gap-2 md:flex-row h-[150px] md:h-[100px]">
+    <nav className="flex flex-col bg-[#222937] text-white px-10 py-4 justify-between items-center gap-2 md:flex-row h-[170px] md:h-[100px]">
       <a href="/" className="flex justify-center items-center gap-3">
         <Image src={imgPath} width={40} />
         <div>
-          <h1 className={`text-2xl md:text-xl font-bold ${poppins.className}`}>Sains Ngawonggo</h1>
-          <p>Mengenal sains dengan lebih seru</p>
+          <h1 className={`text-lg font-bold ${poppins.className}`}>Sains Ngawonggo</h1>
+          <p className="text-sm md:text-md">Mengenal sains dengan lebih seru</p>
         </div>
       </a>
-      <ul className="flex gap-10 font-bold justify-center items-center text-center">
-        <a href="/" className="hover:border-b-[1px] hover:border-b-white p-4 transition-all">
+      <ul className="flex gap-10 font-bold justify-center items-center text-center text-sm md:text-md">
+        <a href="/" className="hover:border-b-[2px] hover:border-b-white p-4 transition-all">
           <li>Beranda</li>
         </a>
-        <a href="/#article" className="hover:border-b-[1px] hover:border-b-white p-4 transition-all">
+        <a href="/#article" className="hover:border-b-[2px] hover:border-b-white p-4 transition-all">
           <li>Artikel</li>
         </a>
-        <a href="" className="hover:border-b-[1px] hover:border-b-white p-4 transition-all">
+        <a href="/" className="hover:border-b-[2px] hover:border-b-white p-4 transition-all">
           <li>Tentang Kami</li>
         </a>
-        <a href="" className="hover:border-b-[1px] hover:border-b-white p-4 transition-all">
+        <a href="/" className="hover:border-b-[2px] hover:border-b-white p-4 transition-all">
           <li>Kontak Kami</li>
         </a>
       </ul>
